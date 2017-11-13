fd731bc3ebba328b94242f894094a19634b13996
Shrikanth <mvnshrikanth@gmail.com>
2017-11-12 23:18:57 -0500

    Updated the design by adding TabView and ViewPager added menu for sign out and editing profile using new profile activity.

--- app/src/main/java/com/android/mvnshrikanth/theblooddonor/ui/MainActivity.java
+++ app/src/main/java/com/android/mvnshrikanth/theblooddonor/ui/MainActivity.java
@@ -1,18 +1,47 @@
 package com.android.mvnshrikanth.theblooddonor.ui;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.android.mvnshrikanth.theblooddonor.R;
 
 public class MainActivity extends AppCompatActivity {
     private static final String LOG_TAG = MainActivity.class.getSimpleName();
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        getFragmentManager().beginTransaction()
+                .add(R.id.fragment_main_container, new MainFragment())
+                .commit();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.item_show_profile:
+                Intent intent = new Intent(this, ProfileActivity.class);
+                startActivity(intent);
+                break;
+            default:
+                //TODO Add code to sign out the user.
+                Toast.makeText(this, "Sign Out", Toast.LENGTH_SHORT).show();
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
