ade7c821297b82467e022d73e7f17aac8a1b662b
Shrikanth <mvnshrikanth@gmail.com>
2017-12-14 14:57:15 -0500

    Added new activities and fragments for the chat functionality.

---
+++ app/src/main/java/com/android/mvnshrikanth/theblooddonor/ui/ChatActivity.java
@@ -0,0 +1,15 @@
+package com.android.mvnshrikanth.theblooddonor.ui;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.android.mvnshrikanth.theblooddonor.R;
+
+public class ChatActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chat);
+    }
+}
---
+++ app/src/main/java/com/android/mvnshrikanth/theblooddonor/ui/ChatMessageFragment.java
@@ -0,0 +1,30 @@
+package com.android.mvnshrikanth.theblooddonor.ui;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.android.mvnshrikanth.theblooddonor.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class ChatMessageFragment extends Fragment {
+
+
+    public ChatMessageFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_chat_message, container, false);
+    }
+
+}
